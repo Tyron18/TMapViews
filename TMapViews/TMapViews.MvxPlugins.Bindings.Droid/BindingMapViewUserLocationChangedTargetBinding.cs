4193f2a99a04bf4be6fa13cd7d5701949f74093b
Tyron De Andrade <tyron.deandrade@fullstack.co.za>
2018-05-23 18:30:17 +0200

    MvvmCross Bindings Plugin

---
+++ TMapViews/TMapViews.MvxPlugins.Bindings.Droid/BindingMapViewUserLocationChangedTargetBinding.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using MvvmCross;
+using MvvmCross.Binding;
+using MvvmCross.Binding.Bindings.Target.Construction;
+using MvvmCross.Platforms.Android.Binding.Target;
+using MvvmCross.Platforms.Android.WeakSubscription;
+using MvvmCross.Plugin;
+using TMapViews.Droid.Views;
+using TMapViews.Models.Models;
+
+namespace TMapViews.MvxPlugins.Bindings.Droid
+{
+    public class BindingMapViewUserLocationChangedTargetBinding : MvxAndroidTargetBinding<BindingMapView, Binding3DLocation>
+    {
+        private MvxAndroidTargetEventSubscription<BindingMapView, Binding3DLocation> subscribeUserLocationChanged;
+
+        public override MvxBindingMode DefaultMode => MvxBindingMode.OneWayToSource;
+
+        public BindingMapViewUserLocationChangedTargetBinding(BindingMapView target) : base(target)
+        {
+        }
+
+        protected override void SetValueImpl(BindingMapView target, Binding3DLocation value)
+        {
+        }
+
+        public override void SubscribeToEvents()
+        {
+            if(Target != null)
+            {
+                subscribeUserLocationChanged = new MvxAndroidTargetEventSubscription<BindingMapView, Binding3DLocation>(Target, nameof(BindingMapView.UserLocationChanged), OnUserLocationChanged);
+            }
+        }
+
+        private void OnUserLocationChanged(object sender, Binding3DLocation e)
+        {
+            if (Target != null)
+                FireValueChanged(Target.UserLocation);
+        }
+
+        protected override void Dispose(bool isDisposing)
+        {
+            if(isDisposing)
+            {
+                subscribeUserLocationChanged?.Dispose();
+                subscribeUserLocationChanged = null;
+            }
+            base.Dispose(isDisposing);
+        }
+
+    }
+}
---
+++ TMapViews/TMapViews.MvxPlugins.Bindings.Droid/Plugin.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using MvvmCross;
+using MvvmCross.Binding.Bindings.Target.Construction;
+using MvvmCross.Plugin;
+using TMapViews.Droid.Views;
+
+namespace TMapViews.MvxPlugins.Bindings.Droid
+{
+    [MvxPlugin]
+    [Preserve(AllMembers = true)]
+    public class Plugin: IMvxPlugin
+    {
+        public void Load()
+        {
+            Mvx.CallbackWhenRegistered<IMvxTargetBindingFactoryRegistry>(RegisterFactories);
+        }
+
+        private void RegisterFactories(IMvxTargetBindingFactoryRegistry registry)
+        {
+
+            registry?.RegisterCustomBindingFactory<BindingMapView>(nameof(BindingMapView.UserLocation), target => new BindingMapViewUserLocationChangedTargetBinding(target));
+        }
+    }
+}
