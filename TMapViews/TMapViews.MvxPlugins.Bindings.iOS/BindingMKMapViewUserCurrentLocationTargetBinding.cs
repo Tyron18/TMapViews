916bf32ddd2c0324217959d9470cc94cb8e5729d
Tyron De Andrade <tyron.deandrade@fullstack.co.za>
2018-05-24 13:00:21 +0200

    iOS mvx binding plugin

---
+++ TMapViews/TMapViews.MvxPlugins.Bindings.iOS/BindingMKMapViewUserCurrentLocationTargetBinding.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using MvvmCross;
+using MvvmCross.Binding;
+using MvvmCross.Binding.Bindings.Target;
+using TMapViews.iOS;
+using TMapViews.Models.Interfaces;
+
+namespace TMapViews.MvxPlugins.Bindings.iOS
+{
+    [Preserve(AllMembers = true)]
+    public class BindingMKMapViewUserCurrentLocationTargetBinding : MvxTargetBinding<BindingMKMapView, I3DLocation>
+    {
+        private bool _subscribed;
+
+        public BindingMKMapViewUserCurrentLocationTargetBinding(BindingMKMapView target) : base(target)
+        {
+        }
+
+        public override MvxBindingMode DefaultMode => MvxBindingMode.OneWayToSource;
+
+        protected override void SetValue(I3DLocation value)
+        {
+        }
+
+        public override void SubscribeToEvents()
+        {
+            base.SubscribeToEvents();
+            if (Target != null)
+            {
+                Target.UserLocationChanged += UserLocationChanged;
+                _subscribed = true;
+                    }
+        }
+
+        protected override void Dispose(bool isDisposing)
+        {
+            base.Dispose(isDisposing);
+            if (Target != null && _subscribed)
+            {
+                Target.UserLocationChanged -= UserLocationChanged;
+                _subscribed = false;
+            }
+        }
+
+        private void UserLocationChanged(object sender, I3DLocation e)
+        {
+            FireValueChanged(Target.UserCurrentLocation);
+        }
+
+    }
+}
---
+++ TMapViews/TMapViews.MvxPlugins.Bindings.iOS/Plugin.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using MvvmCross;
+using MvvmCross.Binding.Bindings.Target.Construction;
+using MvvmCross.Plugin;
+using TMapViews.iOS;
+
+namespace TMapViews.MvxPlugins.Bindings.iOS
+{
+    [MvxPlugin]
+    [Preserve(AllMembers = true)]
+    public class Plugin : IMvxPlugin
+    {
+        public void Load()
+        {
+            Mvx.CallbackWhenRegistered<IMvxTargetBindingFactoryRegistry>(RegisterFactories);
+        }
+
+        private void RegisterFactories(IMvxTargetBindingFactoryRegistry obj)
+        {
+            obj.RegisterCustomBindingFactory<BindingMKMapView>(nameof(BindingMKMapView.UserCurrentLocation), view => new BindingMKMapViewUserCurrentLocationTargetBinding(view));
+        }
+    }
+}
