f1d80be663028b1d7cc1292c435a773332452e29
Tyron De Andrade <tyron.deandrade@fullstack.co.za>
2018-09-26 08:58:21 +0200

    fix missing file include

---
+++ TMapViews/TMapViews.MvxPlugins/TMapViews.MvxPlugins.Bindings.iOS/MvxBindingMKCallout.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Foundation;
+using MvvmCross.Binding.BindingContext;
+using MvvmCross.Platforms.Ios.Binding.Views;
+using TMapViews.iOS.Views;
+using UIKit;
+
+namespace TMapViews.MvxPlugins.Bindings.iOS
+{
+    public abstract class MvxBindingCalloutView : BindingMKCalloutView, IMvxBindable
+    {
+        public IMvxBindingContext BindingContext { get; set; }
+        public object DataContext { get => BindingContext.DataContext; set => BindingContext.DataContext = value; }
+
+        public MvxBindingCalloutView()
+        {
+            this.CreateBindingContext(string.Empty);
+        }
+    }
+}
