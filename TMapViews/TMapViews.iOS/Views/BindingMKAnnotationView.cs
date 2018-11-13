949c9f57ca48b98000f55711994316b19888874a
Tyron De Andrade <tyron.deandrade@fullstack.co.za>
2018-11-13 08:30:08 +0200

    fix missing ios files

---
+++ TMapViews/TMapViews.iOS/Views/BindingMKAnnotationView.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using CoreGraphics;
+using Foundation;
+using MapKit;
+using UIKit;
+
+namespace TMapViews.iOS.Views
+{
+    public class BindingMKAnnotationView : MKAnnotationView
+    {
+        public BindingMKAnnotationView()
+        {
+        }
+
+        public BindingMKAnnotationView(NSCoder coder) : base(coder)
+        {
+        }
+
+        public BindingMKAnnotationView(CGRect frame) : base(frame)
+        {
+        }
+
+        public BindingMKAnnotationView(IMKAnnotation annotation, string reuseIdentifier) : base(annotation, reuseIdentifier)
+        {
+        }
+
+        protected BindingMKAnnotationView(NSObjectFlag t) : base(t)
+        {
+        }
+
+        protected internal BindingMKAnnotationView(IntPtr handle) : base(handle)
+        {
+        }
+
+        public override UIView HitTest(CGPoint point, UIEvent uievent)
+        {
+            var view = base.HitTest(point, uievent);
+            if (view != null)
+                Superview.BringSubviewToFront(this);
+            return view;
+        }
+
+        public override bool PointInside(CGPoint point, UIEvent uievent)
+        {
+            bool result = Bounds.Contains(point);
+            var i = 0;
+            while ( !result && i < Subviews.Count())
+            {
+                var frame = Subviews[i++].Frame;
+                result = frame.Contains(point);
+            }
+            return result;
+        }
+    }
+}
---
+++ TMapViews/TMapViews.iOS/Views/BindingMKCalloutView.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using Cirrious.FluentLayouts.Touch;
+using TMapViews.Models;
+using UIKit;
+
+namespace TMapViews.iOS.Views
+{
+    public class BindingMKCalloutView : UIView
+    {
+        public BindingMKCalloutView()
+        {
+        }
+
+        protected nfloat _xOffset;
+        protected nfloat _yOffset;
+
+        public virtual nfloat XOffset
+        {
+            get => _xOffset;
+            set
+            {
+                _xOffset = value;
+                if (XPosLayout != null)
+                    XPosLayout.Constant = _xOffset;
+            }
+        }
+
+        public virtual nfloat YOffset
+        {
+            get => _yOffset;
+            set
+            {
+                _yOffset = value;
+                if (YPosLayout != null)
+                    YPosLayout.Constant = _yOffset;
+            }
+        }
+
+        internal FluentLayout XPosLayout { get; set; }
+        internal FluentLayout YPosLayout { get; set; }
+
+        internal IBindingMapAnnotation Annotation { get; set; }
+    }
+}
